5ff7e73048db94092a868cdf8826ebd35a472187
jayjaycool208209 <77368450+jayjaycool208209@users.noreply.github.com>
2024-09-05 14:32:53 +0000

    Finish lab2

--- lab2.cpp
+++ lab2.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+int main() {
+    // Assignment 1 - Your Name
+
+    // Declare variables
+    double grade1, grade2, grade3, grade4;
+    double average;
+
+    // Prompt user for input
+    std::cout << “Enter four test grades: “;
+    std::cin >> grade1 >> grade2 >> grade3 >> grade4;
+
+    // Compute the average
+    average = (grade1 + grade2 + grade3 + grade4) / 4.0;
+
+    // Display the average
+    std::cout << “The average grade is: “ << average << std::endl;
+
+    return 0;
+}
